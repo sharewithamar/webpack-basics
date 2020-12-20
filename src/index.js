2eea02a5e5a1601649a234336930d42e02f12db1
Amarnath <sharewithamar@gmail.com>
2020-12-20 13:14:47 +0530

    loader basics

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import addImage from './add-image.js';
+//import helloWorld from './components/hello-world-button/hello-world-button.js';
+import HelloWorldButton from './components/hello-world-button/hello-world-button';
+
+//helloWorld();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
+addImage();
